1bbcbcbbd246c80cc4f1f55b7a1d0168c07df9e2
Oleg Anisimov <oktdianisimov@gmail.com>
2024-03-07 10:05:32 +0300

    release v1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import com.sun.jdi.connect.Connector;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    Connector connector;
+
+
 }
